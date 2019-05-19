33026d730d97b128f0525442641ef8b215bdb5f1
Clouds <Clouds.Flowing@gmail.com>
2019-05-20 02:22:54 +0800

    Create example3.8_gridworld.py

---
+++ example3.8_gridworld.py
@@ -0,0 +1,14 @@
+"""
+Example 3.8: Gridworld
+consider the grid
+  .A.B.
+  .....
+  ...b.
+  .....
+  .a...
+
+* action off grid leaves unchanged with reward -1
+* A -> a with reward 10
+* B -> b with reward 5
+* γ = 0.9
+"""
